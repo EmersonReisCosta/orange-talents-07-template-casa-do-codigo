28e12bf195379a13673669a9d70316de70ce03ea
EmersonReisCosta <emerson.costa@zup.com.br>
2021-07-30 11:02:05 -0300

    criado AutorRepository

--- src/main/java/br/com/zup/emerson/casadocodigo/controller/AutorController.java
+++ src/main/java/br/com/zup/emerson/casadocodigo/controller/AutorController.java
@@ -1,6 +1,16 @@
 package br.com.zup.emerson.casadocodigo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/autores")
 public class AutorController {
+
+    public ResponseEntity<?> cadastrarAutor(){
+
+        return null;
+    }
 
 }
---
+++ src/main/java/br/com/zup/emerson/casadocodigo/repository/AutorRepository.java
@@ -0,0 +1,4 @@
+package br.com.zup.emerson.casadocodigo.repository;
+
+public interface AutorRepository {
+}
