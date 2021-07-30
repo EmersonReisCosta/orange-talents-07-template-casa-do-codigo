0ebfc717e5032ea7ddc864119818ca1632fa507a
EmersonReisCosta <emerson.costa@zup.com.br>
2021-07-30 10:54:58 -0300

    configurado dialeto MySQL

--- src/main/java/br/com/zup/emerson/casadocodigo/model/Autor.java
+++ src/main/java/br/com/zup/emerson/casadocodigo/model/Autor.java
@@ -1,9 +1,11 @@
 package br.com.zup.emerson.casadocodigo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -13,12 +15,34 @@ public class Autor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
+    @Column(length = 400)
     private String descricao;
+
+    @NotNull
+    @CreationTimestamp
     private LocalDate instante;
+
+
 
     public Long getId() {
         return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public LocalDate getInstante() {
+        return instante;
     }
 }
