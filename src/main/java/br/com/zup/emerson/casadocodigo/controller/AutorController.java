545e25c40611813b87e5c90174d5dab7ecfe2218
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-03 10:33:46 -0300

    adicionado logica para evitar email duplicado

--- src/main/java/br/com/zup/emerson/casadocodigo/controller/AutorController.java
+++ src/main/java/br/com/zup/emerson/casadocodigo/controller/AutorController.java
@@ -5,6 +5,7 @@ import br.com.zup.emerson.casadocodigo.model.Autor;
 import br.com.zup.emerson.casadocodigo.repository.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -22,15 +23,27 @@ public class AutorController {
 
 
     @PostMapping
-    public Autor cadastrarAutor(@RequestBody @Valid AutorForm autorForm){
+    public Autor cadastrarAutor(@RequestBody @Valid AutorForm autorForm, Errors errors){
         Autor autor = autorForm.converter();
-
-
+        validaEmail(autor, errors);
         autorRepository.save(autor);
-
-
         return autor;
+    }
 
+    public void validaEmail(Autor autor, Errors errors){
+
+        if (errors.hasErrors()){
+            return;
+        }
+
+        Optional<Autor> emailExistente = autorRepository
+                .findByEmail(autor.getEmail());
+
+        if(emailExistente != null && !emailExistente.equals(autor)) {
+            errors.rejectValue("email", null,
+                    "Já existe um(a) outro(a) autor(a) com o mesmo email "
+                            + autor.getEmail());
+        }
     }
 
 }
--- src/main/java/br/com/zup/emerson/casadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zup/emerson/casadocodigo/repository/AutorRepository.java
@@ -2,6 +2,12 @@ package br.com.zup.emerson.casadocodigo.repository;
 
 import br.com.zup.emerson.casadocodigo.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    Optional<Autor> findByEmail(String email);
 }
