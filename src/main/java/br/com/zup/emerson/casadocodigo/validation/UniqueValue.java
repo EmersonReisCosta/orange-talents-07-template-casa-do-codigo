3595c426172f51ca6ba4b575c9406dd57321e2a4
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-05 09:06:47 -0300

    criado validation UniqueValue

--- src/main/java/br/com/zup/emerson/casadocodigo/validation/UniqueValue.java
+++ src/main/java/br/com/zup/emerson/casadocodigo/validation/UniqueValue.java
@@ -1,14 +1,21 @@
 package br.com.zup.emerson.casadocodigo.validation;
 
 import javax.validation.Constraint;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.lang.reflect.Field;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
 @Documented
-@Constraint(Vali)
-@Target({FIELD})
-@Retention(RUNTIME)
+@Constraint()
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
 public @interface UniqueValue {
+    public String message() default "O campo deve ser unico";
+    public Class<?>[] groups() default {};
+    public Class<? extends Payload>[] payload() default{};
+    String fieldName();
+    Class<?> domainClass();
 }
