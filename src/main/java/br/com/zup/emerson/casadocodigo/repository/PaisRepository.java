dbe25739f513f252a774b29350333bb29dc78606
EmersonReisCosta <emerson.costa@zup.com.br>
2021-08-10 10:04:26 -0300

    Criado paisRepository

---
+++ src/main/java/br/com/zup/emerson/casadocodigo/repository/PaisRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.emerson.casadocodigo.repository;
+
+import br.com.zup.emerson.casadocodigo.model.Pais;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaisRepository extends JpaRepository<Pais, Long> {
+
+}
